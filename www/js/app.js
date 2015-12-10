8f34ca11c1cd9d96e31c4a3ed248ff44cdc6489f
Andrew Anissi <andrew@wingedearth.com>
2015-12-10 10:06:04 -0800

    first commit with starter code

---
+++ www/js/app.js
@@ -0,0 +1,42 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.play', {
+    url: '/play',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/play.html'
+      }
+    }
+  });
+
+  // Default route
+  $urlRouterProvider.otherwise('/app/play');
+});
